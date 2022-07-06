6c4da78593f555e075da43348d6964cee926e392
testacc1119 <Dyggasus@gmail.com>
2022-07-06 11:12:55 +0300

    starting point

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,17 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+package dyhas.bo.lab1
 
-def msg = "I was compiled by Scala 3. :)"
+import scala.annotation.tailrec
+
+
+def foldLeft[A, B](xs: Seq[A], z: B)(op: (B, A) => B): B = 
+  @tailrec
+  def f(xs: Seq[A], acc: B): B = xs match {
+    case Seq()   => acc
+    case x +: xs => f(xs, op(acc, x))
+  }
+  f(xs, z)
+
+
+@main def hello: Unit = 
+  val l = List(1, 2, 3, 4)
+  println(foldLeft(l,0)(_ + _))
