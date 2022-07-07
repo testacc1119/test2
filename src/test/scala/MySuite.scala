ed5e96d6515d48a106b7a74fa2b766831ce2c55a
testacc1119 <Dyggasus@gmail.com>
2022-07-07 16:28:09 +0300

    reorganized tests

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -4,36 +4,10 @@ import munit.Clue.generate
 
 class MySuite extends munit.FunSuite {
 
-test("List.foldLeft (Int)"){
-  val expected = 600
-  val actual = List(1,2,3,4,5).foldLeft(5)(_*_)
-  assertEquals(expected,actual)
-}
-
-test("List.foldLeft (String)"){
-  val expected = "123abcde"
-  val actual = List("a","b", "c", "d", "e").foldLeft("123")(_+_)
-  assertEquals(actual, expected)
-}
-
 test("List constructor"){
   val expected = List(1)
   var z = 1
   val actual = List(z)
-  assertEquals(actual, expected)
-}
-
-test("List.grouped"){
-  val expected = List(List(0, 1), List(2, 3))
-  var temp = List(0, 1, 2, 3)
-  val actual = temp.grouped(2)
-  assertEquals(actual, expected)
-}
-
-test("List.sliding"){
-  val expected = List(List(1, 2, 3), List(2, 3, 4))
-  var temp = List(1, 2, 3, 4)
-  val actual = temp.sliding(3)
   assertEquals(actual, expected)
 }
 
@@ -97,4 +71,31 @@ test("ListofLists.add_right empty"){
   val actual = List.of().add_right(List(1))
   assertEquals(actual, expected)
 }
+
+test("List.grouped"){
+  val expected = List(List(0, 1), List(2, 3))
+  var temp = List(0, 1, 2, 3)
+  val actual = temp.grouped(2)
+  assertEquals(actual, expected)
+}
+
+test("List.sliding"){
+  val expected = List(List(1, 2, 3), List(2, 3, 4))
+  var temp = List(1, 2, 3, 4)
+  val actual = temp.sliding(3)
+  assertEquals(actual, expected)
+}
+
+test("List.foldLeft (Int)"){
+  val expected = 600
+  val actual = List(1,2,3,4,5).foldLeft(5)(_*_)
+  assertEquals(expected,actual)
+}
+
+test("List.foldLeft (String)"){
+  val expected = "123abcde"
+  val actual = List("a","b", "c", "d", "e").foldLeft("123")(_+_)
+  assertEquals(actual, expected)
+}
+
 }
